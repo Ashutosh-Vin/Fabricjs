7f6ab10142b7d4aaaf6bab262d6706cf41a76b42
Ashutosh-Vin <ashutoshsrivastava177@gmail.com>
2022-05-07 16:02:34 +0530

    set maximum zoom out aut and point to zoom

--- src/js/main.js
+++ src/js/main.js
@@ -15,8 +15,7 @@ function addFileOnCanvas(e) {
         oImg.scaleToWidth(400);
         canvas.add(oImg).renderAll();
         var a = canvas.setActiveObject(oImg);
-        var dataURL = canvas.toDataURL({format: 'jpg', quality: 0.8});
-        
+        var dataURL = canvas.toDataURL({format: 'jpg', quality: 0.8});       
       });
     };
     reader.readAsDataURL(file);
@@ -25,39 +24,50 @@ function addFileOnCanvas(e) {
   canvas.on('mouse:wheel', zoomImage);
 
   function zoomImage(opt){
+    var target = canvas.findTarget(opt);
+    console.log('target : ',target);
+    
     var delta = opt.e.deltaY;
     var zoom = canvas.getZoom();
+    console.log('deltay :',delta)
+    console.log('zoom :',canvas.getZoom())
     zoom *= 0.999 ** delta;
-    if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
-    canvas.setZoom(zoom);
+    if((delta > 0) && (zoom < 1)){
+      zoom = 1;
+    }
+    canvas.zoomToPoint(
+      new fabric.Point(opt.e.offsetX, opt.e.offsetY),
+      zoom,
+  );
+    //canvas.setZoom(zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
   }
-  canvas.on('mouse:down', function(opt) {
-    var evt = opt.e;
-    if (evt.altKey === true) {
-      this.isDragging = true;
-      this.selection = false;
-      this.lastPosX = evt.clientX;
-      this.lastPosY = evt.clientY;
-    }
-  });
-  canvas.on('mouse:move', function(opt) {
-    if (this.isDragging) {
-      var e = opt.e;
-      var vpt = this.viewportTransform;
-      vpt[4] += e.clientX - this.lastPosX;
-      vpt[5] += e.clientY - this.lastPosY;
-      this.requestRenderAll();
-      this.lastPosX = e.clientX;
-      this.lastPosY = e.clientY;
-    }
-  });
-  canvas.on('mouse:up', function(opt) {
-    // on mouse up we want to recalculate new interaction
-    // for all objects, so we call setViewportTransform
-    this.setViewportTransform(this.viewportTransform);
-    this.isDragging = false;
-    this.selection = true;
-  });
+
+  // canvas.on('mouse:down', function(opt) {
+  //   var evt = opt.e;
+  //   if (evt.altKey === true) {
+  //     this.isDragging = true;
+  //     this.selection = false;
+  //     this.lastPosX = evt.clientX;
+  //     this.lastPosY = evt.clientY;
+  //   }
+  // });
+  // canvas.on('mouse:move', function(opt) {
+  //   if (this.isDragging) {
+  //     var e = opt.e;
+  //     var vpt = this.viewportTransform;
+  //     vpt[4] += e.clientX - this.lastPosX;
+  //     vpt[5] += e.clientY - this.lastPosY;
+  //     this.requestRenderAll();
+  //     this.lastPosX = e.clientX;
+  //     this.lastPosY = e.clientY;
+  //   }
+  // });
+  // canvas.on('mouse:up', function(opt) {
+  //   // on mouse up we want to recalculate new interaction
+  //   // for all objects, so we call setViewportTransform
+  //   this.setViewportTransform(this.viewportTransform);
+  //   this.isDragging = false;
+  //   this.selection = true;
+  // });
